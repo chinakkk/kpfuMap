86634d66b179941cd4df7f8c7f3d50004e162b68
chinakkk <chingizkeldibekov@gmail.com>
2022-04-26 23:15:44 +0300

    lines

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
-console.log("goodjobman")
-console.log("what")
-console.log("1")
+var way=document.getElementsByClassName('way')
+
+
+var space=32;
+var numWay
+
+
+
+
+document.addEventListener("keydown",keyPressFun)
+function keyPressFun(event){
+
+    if (event.keyCode==space) {
+        // way1.style.background="black"
+        numWay=Number(document.getElementById('inputWay').value)
+        if(numWay!=0&&numWay<way.length+1)
+            editColor(numWay)
+        document.getElementById('inputWay').value=''
+    }
+
+}
+function editColor(numWay){
+    way[numWay-1].style.background="black"
+    
+}
+function giveNumWay(){
+    numWay = Number(document.getElementById('inputWay').value)
+    return numWay
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
