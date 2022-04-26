44e99f8b2e07c7d17a0431cd679509497590825a
chinakkk <chingizkeldibekov@gmail.com>
2022-04-27 01:37:53 +0300

    ЛинииПоявляютсяПоВводуЦифр

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var way=document.getElementsByClassName('way')
+var invis=document.getElementsByClassName('invis')
 
 
 var space=32;
@@ -11,16 +11,15 @@ document.addEventListener("keydown",keyPressFun)
 function keyPressFun(event){
 
     if (event.keyCode==space) {
-        // way1.style.background="black"
         numWay=Number(document.getElementById('inputWay').value)
-        if(numWay!=0&&numWay<way.length+1)
+        if(numWay!=0&&numWay<invis.length+1)
             editColor(numWay)
         document.getElementById('inputWay').value=''
     }
 
 }
 function editColor(numWay){
-    way[numWay-1].style.background="black"
+    invis[numWay-1].style.opacity=1
     
 }
 function giveNumWay(){
