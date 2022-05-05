1e301a390e90f60a919cdea6b288911b331dff91
chinakkk <chingizkeldibekov@gmail.com>
2022-05-06 01:21:32 +0300

    РеализовалНедоАнимациюКвадрата

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ const canvas=document.getElementById("canvasMap")
 const ctx=canvas.getContext("2d")
 
 
-var numWay;//номер который вводит пользователь в строку
-var space=32;//пробел
+const backgroundMap=new Image();
+// backgroundMap.src="/jpegForm.jpg"
+backgroundMap.src="map.gif"
 
-var way=new Array()
-var way=[true,true,true,true,true,true,true]//тестовый массив для отключения прорисовки красных линий
+var numWay=0;//номер который вводит пользователь в строку
+var space=32;//пробел
+var z=90;
+var pi=Math.PI
+
+var way={
+  x:920,
+  y:580,
+};
+//489
 
 class Graph {
     constructor() {
@@ -179,144 +188,15 @@ class Graph {
 
 
 }
-//красные линии
-function drawRedLines(numWay){
-
-    ctx.fillStyle="rgb(192, 248, 229)";
-    ctx.fillRect(0,0,2000,2000);
-
-    way[numWay]=false
-
-    ctx.fillStyle="red";
-    if(way[0])ctx.fillRect(100,0,10,500);
-    if(way[1])ctx.fillRect(150,0,10,500);
-    if(way[2])ctx.fillRect(200,0,10,500);
-    if(way[3])ctx.fillRect(250,0,10,500);
-    if(way[4])ctx.fillRect(300,0,10,500);
-    if(way[5])ctx.fillRect(350,0,10,500);
-    if(way[6])ctx.fillRect(400,0,10,500);
-
-    drawText("1",80,20)
-    drawText("2",130,20)
-    drawText("3",180,20)
-    drawText("4",230,20)
-    drawText("5",280,20)
-    drawText("6",330,20)
-    drawText("7",380,20)
-
-}
-//стереть красные линии
-function drawRedLineRestart(){
-  for(let i=0;i<way.length;i++)
-  {
-      way[i]=true
-  }
-}
-
-
-
-//прорисовка определенной верви
-function drawWayTree(finalNum){
-
-  let arrayWay=new Array()
-  arrayWay=graph.findShortestPath('1', finalNum)
-  canvasClear()
-  for(var i=0;i<arrayWay.length;i++){
-    if(arrayWay[i]==1){
-      drawLine(10, 250,100, 250);//1
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==2){
-      drawLine(100, 250,100, 150);//2
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==3){
-      drawLine(100, 150,200, 150);//3
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==4){
-      drawLine(200, 150,200, 100);//4
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==5){
-      drawLine(200, 100,250, 100);//5
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==6){
-      drawLine(200, 150,200, 200);//6
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==7){
-      drawLine(200, 200,250, 200);//7
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==8){
-      drawLine(100, 250,100, 350);//8
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==9){
-      drawLine(100, 350,200, 350);//9
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==10){
-      drawLine(200, 350,200, 400);//10
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==11){
-      drawLine(200, 400,250, 400);//11
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==12){
-      drawLine(200, 350,200, 300);//12
-      // console.log(arrayWay[i])
-    }
-    if(arrayWay[i]==13){
-      drawLine(200, 300,250, 300);//13
-      // console.log(arrayWay[i])
-    }
-  }
-  drawNumber()
-  
-}
-//прорисовка всего терева
-function drawWayPic(){
-  drawLine(10, 250,100, 250);
-  drawLine(100, 250,100, 150)
-  drawLine(100, 150,200, 150)
-  drawLine(200, 150,200, 100)
-  drawLine(200, 100,250, 100)
-  drawLine(200, 150,200, 200)
-  drawLine(200, 200,250, 200)
-  drawLine(100, 250,100, 350)
-  drawLine(100, 350,200, 350)
-  drawLine(200, 350,200, 400)
-  drawLine(200, 400,250, 400)
-  drawLine(200, 350,200, 300)
-  drawLine(200, 300,250, 300)
-}
-//прорисовка цифр на дереве
-function drawNumber(){
-
-  drawText("1",80,240)
-  drawText("2",80,140)
-  drawText("3",180,140)
-  drawText("4",190,90)
-  drawText("5",240,90)
-  drawText("6",190,220)
-  drawText("7",240,220)
-  drawText("8",80,380)
-  drawText("9",180,380)
-  drawText("10",180,430)
-  drawText("11",230,430)
-  drawText("12",180,290)
-  drawText("13",230,290)
-}
 
 
 //прорисовка заднего фона
-function drawBackground(sizeOne=2000,sizeTwo=2000){
+function drawBackground(sizeOne=canvas.height,sizeTwo=canvas.width){
     ctx.fillStyle="rgb(192, 248, 229)";
     ctx.fillRect(0,0,sizeOne,sizeTwo);
+}
+function drawMap(){
+  ctx.drawImage(backgroundMap,0,0,backgroundMap.width*0.95,backgroundMap.height*0.95)
 }
 //обнулить канвас 
 function canvasClear(){
@@ -329,29 +209,167 @@ function drawText(text,x,y,color="black"){
     ctx.fillText(text,x,y);
 }
 //шаблон для линии
-function drawLine(xOne,yOne,xTwo,yTwo,width=2,color='black'){
+function drawLine(xOne,yOne,xTwo,yTwo,width=0.1,color='rgb(121, 234, 196)'){
     ctx.lineWidth = width; 
     ctx.moveTo(xOne, yOne); 
     ctx.lineTo(xTwo, yTwo); 
     ctx.strokeStyle = color;
     ctx.stroke();
 }
-//////////////////нажатие
+function drawCircle(x=way.x,y=way.y,radius=4,start=0,finish=pi*2,watch=true,color='rgb(121, 234, 196)'){
+  // ctx.lineWidth=width
+  ctx.strokeStyle=color
+  ctx.arc(x,y,radius,start,finish,watch)
+  ctx.stroke()
+}
+function drawRect(x=way.x,y=way.y,sizeOne=10,sizeTwo=10,color='rgb(121, 234, 196)'){
+  ctx.fillStyle=color
+  ctx.fillRect(x ,y,sizeOne,sizeTwo)
+}
+function drawSetka(){
+  for(var i=0;i<canvas.width;i+=10)
+  {
+    var yMin=0
+    var yMax=canvas.height
+    drawLine(i,yMin,i,yMax,0.1,'rgb(151, 151, 151)')
+
+    ctx.lineWidth = 1; 
+    ctx.moveTo(i, yMin); 
+    ctx.lineTo(i, yMax); 
+    ctx.strokeStyle = 'rgb(151, 151, 151)';
+    ctx.stroke();
+  }
+  for(var i=0;i<canvas.height;i+=10)
+  {
+    
+    var yMin=0
+    var yMax=canvas.width
+    drawLine(yMin,i,yMax,i,0.1,'rgb(151, 151, 151)')
+
+
+    ctx.lineWidth = 1; 
+    ctx.moveTo(yMin, i); 
+    ctx.lineTo(yMax, i); 
+    ctx.strokeStyle = 'rgb(151, 151, 151)';
+    ctx.stroke();
+  }
+}
+function upWay(sizePx=0){
+  for(var i=way.y-sizePx;i<way.y;way.y--){
+    // drawRect()
+    drawCircle()
+  }
+}
+function downWay(sizePx=0){
+  for(var i=way.y+sizePx;i>way.y;way.y++){
+    // drawRect()
+    drawCircle()
+  }
+}
+function rightWay(sizePx=0){
+  for(var i=way.x+sizePx;i>way.x;way.x++){
+    // drawRect()
+    drawCircle()
+  }
+}
+function leftWay(sizePx=0){
+  for(var i=way.x-sizePx;i<way.x;way.x--){
+    // drawRect()
+    drawCircle()
+  }
+}
+
+function searchRoom(finalNum){
+
+  canvasClear()
+  drawMap()
+  
+  let arrayWay=new Array()
+  arrayWay=graph.findShortestPath('0', finalNum)
+  console.log(arrayWay)
+  for(var i=0;i<arrayWay.length;i++){
+    if(arrayWay[i]==1)upWay(86)
+    if(arrayWay[i]==2)rightWay(83)
+    if(arrayWay[i]==3)rightWay(45)
+    if(arrayWay[i]==4)rightWay(45)
+    if(arrayWay[i]==5)drawLine()
+    if(arrayWay[i]==6)drawLine()
+    if(arrayWay[i]==7)drawLine()
+    if(arrayWay[i]==8)drawLine()
+    if(arrayWay[i]==9)drawLine()
+    if(arrayWay[i]==10)drawLine()
+    if(arrayWay[i]==11)drawLine()
+    if(arrayWay[i]==12)drawLine()
+    if(arrayWay[i]==14)drawLine()
+    if(arrayWay[i]==14)drawLine()
+    if(arrayWay[i]==15)drawLine()
+    if(arrayWay[i]==16)drawLine()
+    if(arrayWay[i]==17)drawLine()
+    if(arrayWay[i]==18)drawLine()
+    if(arrayWay[i]==19)drawLine()
+    if(arrayWay[i]==20)drawLine()
+    if(arrayWay[i]==21)drawLine()
+    if(arrayWay[i]==22)drawLine()
+    if(arrayWay[i]==23)drawLine()
+    if(arrayWay[i]==24)drawLine()
+
+    if(arrayWay[i]==129)upWay(10)
+    if(arrayWay[i]==130)upWay(10)
+    if(arrayWay[i]==131)upWay(10)
+    if(arrayWay[i]==132)upWay(10)
+    if(arrayWay[i]==133)upWay(10)
+    if(arrayWay[i]==134)upWay(10)
+    if(arrayWay[i]==135)upWay(10)
+    if(arrayWay[i]==136)upWay(10)
+    if(arrayWay[i]==137)upWay(10)
+    if(arrayWay[i]==138)drawLine();
+    if(arrayWay[i]==139)drawLine();
+    if(arrayWay[i]==140)drawLine();
+    if(arrayWay[i]==141)drawLine();
+    if(arrayWay[i]==142)drawLine();
+    if(arrayWay[i]==143)drawLine();
+    if(arrayWay[i]==144)drawLine();
+    if(arrayWay[i]==145)drawLine();
+    if(arrayWay[i]==146)drawLine();
+    if(arrayWay[i]==147)drawLine();
+    if(arrayWay[i]==148)drawLine();
+    if(arrayWay[i]==149)drawLine();
+    if(arrayWay[i]==150)drawLine();
+    if(arrayWay[i]==151)drawLine();
+    if(arrayWay[i]==152)drawLine();
+    if(arrayWay[i]==153)drawLine();
+    if(arrayWay[i]==154)drawLine();
+    if(arrayWay[i]==155)drawLine();
+    if(arrayWay[i]==156)drawLine();
+    if(arrayWay[i]==157)drawLine();
+    if(arrayWay[i]==158)drawLine();
+    if(arrayWay[i]==159)drawLine();
+
+    
+  }
+}
+
+
+//нажатие
 document.addEventListener("keydown",pressKey)
 function pressKey(event){
   if(event.keyCode==space){
     numWay=Number(document.getElementById('inputWay').value)
     document.getElementById('inputWay').value=''
-    if(numWay!=1)drawWayTree(numWay)
-    else if(numWay==1){
-      canvasClear()
-      drawLine(10, 250,100, 250);
-      drawNumber()
+    if(numWay>0){
+      searchRoom(numWay)
     }
   }
+  if(event.keyCode==z){
+    drawSetka()
+  }
+  searchRoom(135)
 } 
 
+
+
 let graph = new Graph();
+graph.addVertex('0');
 graph.addVertex('1');
 graph.addVertex('2');
 graph.addVertex('3');
@@ -365,28 +383,94 @@ graph.addVertex('10');
 graph.addVertex('11');
 graph.addVertex('12');
 graph.addVertex('13');
+graph.addVertex('14');
+graph.addVertex('15');
 
+//аудитории
+graph.addVertex('129');
+graph.addVertex('130');
+graph.addVertex('131');
+graph.addVertex('132');
+graph.addVertex('133');
+graph.addVertex('134');
+graph.addVertex('135');
+graph.addVertex('136');
+graph.addVertex('137');
+graph.addVertex('138');
+graph.addVertex('139');
+graph.addVertex('140');
+graph.addVertex('141');
+graph.addVertex('142');
+graph.addVertex('143');
+graph.addVertex('144');
+graph.addVertex('145');
+graph.addVertex('146');
+graph.addVertex('147');
+graph.addVertex('148');
+graph.addVertex('149');
+graph.addVertex('150');
+graph.addVertex('151');
+graph.addVertex('152');
+graph.addVertex('153');
+graph.addVertex('154');
+graph.addVertex('155');
+graph.addVertex('156');
+graph.addVertex('157');
+graph.addVertex('158');
+graph.addVertex('159');
+
+//путь
+graph.addEdge('0', '1');
 graph.addEdge('1', '2');
 graph.addEdge('2', '3');
 graph.addEdge('3', '4');
-graph.addEdge('3', '6');
 graph.addEdge('4', '5');
+graph.addEdge('5', '6');
 graph.addEdge('6', '7');
-graph.addEdge('1', '8');
+graph.addEdge('7', '8');
 graph.addEdge('8', '9');
-graph.addEdge('9', '12');
 graph.addEdge('9', '10');
-graph.addEdge('12', '13');
 graph.addEdge('10', '11');
+graph.addEdge('11', '12');
+graph.addEdge('12', '13');
+graph.addEdge('13', '14');
+graph.addEdge('14', '15');
 
-drawWayPic()
-drawNumber()
+//путь к аудиториям
+graph.addEdge('2', '129');
+graph.addEdge('2', '130');
+graph.addEdge('3', '131');
+graph.addEdge('3', '132');
+graph.addEdge('4', '133');
+graph.addEdge('4', '134');
+graph.addEdge('5', '135');
+graph.addEdge('5', '136');
+graph.addEdge('6', '137');
+graph.addEdge('6', '138');
+graph.addEdge('7', '139');
+graph.addEdge('8', '140');
+graph.addEdge('8', '141');
+graph.addEdge('8', '146');
+graph.addEdge('9', '143');
+graph.addEdge('9', '144');
+graph.addEdge('9', '145');
+graph.addEdge('9', '147');
+graph.addEdge('9', '148');
+graph.addEdge('9', '149');
+graph.addEdge('10', '153');
+graph.addEdge('10', '150');
+graph.addEdge('11', '151');
+graph.addEdge('11', '152');
+graph.addEdge('12', '154');
+graph.addEdge('13', '155');
+graph.addEdge('14', '157');
+graph.addEdge('15', '158');
 
 
+// document.addEventListener("DOMContentLoaded", function(){
+// })
 
-
-
-
+backgroundMap.onload=drawMap
 
 
 
